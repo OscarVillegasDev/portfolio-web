856cb86fd47ea8bd847b2e23817ca5a9ac89c12b
OscarVillegasDev <oscare.villegasm@gmail.com>
2025-07-24 14:09:31 -0600

    gif and driver ++

--- driver.js
+++ driver.js
@@ -3,11 +3,12 @@ const driver = window.driver.js.driver;
 
 const driverObj = driver({
   showProgress: true,
+  allowClose: false,
   steps: [
-    { element: '.postman-container', popover: { title: 'Portfolio Tour', description: 'This interface simulates an API request to fetch portfolio data.', side: "left", align: 'start' }},
+    { popover: { title: 'Portfolio Tour', description: 'This interface simulates an API request to fetch portfolio data.' } },
     { element: '#send-button', popover: { title: 'Interact', description: 'Click this button to fetch the portfolio data.', side: "bottom", align: 'start' }},
-    { element: '.response-section', popover: { title: 'Response', description: 'After clicking the send button, the portfolio data will be displayed in json format.', side: "bottom", align: 'start' }},
-    { popover: { title: 'Thats it!', description: 'Some data can be copied by clicking. Welcome to my portfolio!' } }
+    { element: '.response-section', popover: { title: 'Response', description: 'After clicking the send button, the portfolio data will be displayed in json format.', side: "top", align: 'start' }},
+    { popover: { title: 'Thats it!', description: "<img src='./bobGif.gif' style='height: 202.5px; width: 270px;' /><span style='font-size: 15px; display: block; margin-top: 10px; text-align: center;'>Some data can be copied by clicking. Welcome to my portfolio!</span>" } }
   ]
 });
 
